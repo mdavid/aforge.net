7e8eb8cb3971cda08ebd8aa7fa9dcebd8a89aded
andrew.kirillov <andrew.kirillov@ab5b2c17-5f1b-0410-93cd-b5c26ce635c7>
2007-07-26 18:35:46 +0000

    Initial commit of AForge.Video.DirectShow namespace. Classes for enumeration filters are added.
    git-svn-id: http://aforge.googlecode.com/svn/branches/Video.DirectShow@297 ab5b2c17-5f1b-0410-93cd-b5c26ce635c7

---
+++ Sources/Video.DirectShow/Internals/Uuids.cs
@@ -0,0 +1,29 @@
+// AForge Direct Show Library
+// AForge.NET framework
+//
+// Copyright © Andrew Kirillov, 2007
+// andrew.kirillov@gmail.com
+//
+
+namespace AForge.Video.DirectShow.Internals
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+	/// <summary>
+	/// Direct Show class IDs
+	/// </summary>
+    [ComVisible( false )]
+    public class Clsid
+    {
+        /// <summary>
+        /// System device enumerator.
+        /// </summary>
+        /// 
+        /// <remarks>Equals to CLSID_SystemDeviceEnum.</remarks>
+        /// 
+        public static readonly Guid SystemDeviceEnum =
+            new Guid( 0x62BE5D10, 0x60EB, 0x11D0, 0xBD, 0x3B, 0x00, 0xA0, 0xC9, 0x11, 0xCE, 0x86 );
+
+    }
+}
