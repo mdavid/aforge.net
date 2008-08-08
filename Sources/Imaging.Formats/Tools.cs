adf1b1cb3a6d14c84834e908030ea308933b741f
andrew.kirillov <andrew.kirillov@ab5b2c17-5f1b-0410-93cd-b5c26ce635c7>
2008-08-08 20:01:57 +0000

    AForge.Imaging.Formats.IImageDecoder interface is extended with methods, which allow decoding image formats with multiple frame and providing information about decoded frames.
    ReadStream() function is added to guarantee requested number of bytes is read (or end of stream was reached).
    
    git-svn-id: http://aforge.googlecode.com/svn/branches/2.0@595 ab5b2c17-5f1b-0410-93cd-b5c26ce635c7

--- Sources/Imaging.Formats/Tools.cs
+++ Sources/Imaging.Formats/Tools.cs
@@ -10,6 +10,7 @@ namespace AForge.Imaging.Formats
     using System;
     using System.Drawing;
     using System.Drawing.Imaging;
+    using System.IO;
 
     /// <summary>
     /// Set of tools used internally in AForge.Imaging.Formats library.
@@ -35,7 +36,7 @@ namespace AForge.Imaging.Formats
             Bitmap image = new Bitmap( width, height, PixelFormat.Format8bppIndexed );
             // get palette
             ColorPalette cp = image.Palette;
-            // init palette with grayscale color
+            // init palette with grayscale colors
             for ( int i = 0; i < 256; i++ )
             {
                 cp.Entries[i] = Color.FromArgb( i, i, i );
@@ -44,6 +45,43 @@ namespace AForge.Imaging.Formats
             image.Palette = cp;
             // return new image
             return image;
+        }
+
+        /// <summary>
+        /// Read specified amount of bytes from the specified stream.
+        /// </summary>
+        /// 
+        /// <param name="stream">Source sream to read data from.</param>
+        /// <param name="buffer">Buffer to read data into.</param>
+        /// <param name="offset">Offset in buffer to put data into.</param>
+        /// <param name="count">Number of bytes to read.</param>
+        /// 
+        /// <returns>Returns total number of bytes read. It may be smaller than requested amount only
+        /// in the case if end of stream was reached.</returns>
+        /// 
+        /// <remarks><para>This tool function guarantees that requested number of bytes
+        /// was read from the source stream (.NET streams don't guarantee this and may return less bytes
+        /// than it was requested). Only in the case if end of stream was reached, the function
+        /// may return with less bytes read.</para>
+        /// </remarks>
+        /// 
+        public static int ReadStream( Stream stream, byte[] buffer, int offset, int count )
+        {
+            int totalBytesRead = 0, bytesRead = 0;
+
+            while ( totalBytesRead != count )
+            {
+                bytesRead = stream.Read( buffer, offset + totalBytesRead, count - totalBytesRead );
+
+                if ( bytesRead == 0 )
+                {
+                    break;
+                }
+
+                totalBytesRead += bytesRead;
+            }
+
+            return totalBytesRead;
         }
     }
 }
