f336c1c67f489e2da0c84234f379cf1dff4a6eda
andrew.kirillov <andrew.kirillov@ab5b2c17-5f1b-0410-93cd-b5c26ce635c7>
2008-08-29 14:46:21 +0000

    Very first draft of BaseFilter abstract class ....
    git-svn-id: http://aforge.googlecode.com/svn/branches/2.0@630 ab5b2c17-5f1b-0410-93cd-b5c26ce635c7

---
+++ Sources/Imaging/Filters/Base classes/BaseFilter.cs
@@ -0,0 +1,37 @@
+﻿// AForge Image Processing Library
+// AForge.NET framework
+//
+// Copyright © Andrew Kirillov, 2005-2008
+// andrew.kirillov@gmail.com
+//
+
+namespace AForge.Imaging.Filters
+{
+    using System;
+    using System.Drawing;
+    using System.Drawing.Imaging;
+
+    public abstract class BaseFilter : IFilter
+    {
+        public Bitmap Apply( Bitmap image )
+        {
+            return null;
+        }
+
+        public Bitmap Apply( BitmapData imageData )
+        {
+            return null;
+        }
+
+        public UnmanagedImage Apply( UnmanagedImage image )
+        {
+            return null;
+        }
+
+        public void Apply( UnmanagedImage sourceImage, UnmanagedImage destinationImage )
+        {
+        }
+
+        protected abstract unsafe void ProcessFilter( UnmanagedImage sourceData, UnmanagedImage destinationData );
+    }
+}
