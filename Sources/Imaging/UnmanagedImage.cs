4f49ab619456c83bc417c49f851821455f20e487
andrew.kirillov <andrew.kirillov@ab5b2c17-5f1b-0410-93cd-b5c26ce635c7>
2008-08-31 10:11:35 +0000

    UnmanagedImage class is extended with Create() method, which allows to allocate new image in unmanaged memory. The class implements IDisposable interface.
    git-svn-id: http://aforge.googlecode.com/svn/branches/2.0@637 ab5b2c17-5f1b-0410-93cd-b5c26ce635c7

--- Sources/Imaging/UnmanagedImage.cs
+++ Sources/Imaging/UnmanagedImage.cs
@@ -15,9 +15,13 @@ namespace AForge.Imaging
     /// Image in unmanaged memory.
     /// </summary>
     /// 
-    /// <remarks></remarks>
+    /// <remarks>
+    /// <para>The class represents wrapper of an image in unmanaged memory. Using this class
+    /// it is possible as to allocate new image in unmanaged memory, as to just wrap provided
+    /// pointer to unmanaged memory, where an image is stored.</para>
+    /// </remarks>
     /// 
-    public class UnmanagedImage
+    public class UnmanagedImage : IDisposable
     {
         // pointer to image data in unmanaged memory
         private IntPtr imageData;
@@ -27,6 +31,8 @@ namespace AForge.Imaging
         private int stride;
         // image pixel format
         private PixelFormat pixelFormat;
+        // flag which indicates if the image should be disposed or not
+        private bool mustBeDisposed = false;
 
         /// <summary>
         /// Pointer to image data in unmanaged memory.
@@ -78,6 +84,10 @@ namespace AForge.Imaging
         /// <param name="stride">Image stride (line size in bytes).</param>
         /// <param name="pixelFormat">Image pixel format.</param>
         /// 
+        /// <remarks><para><note>Using this constructor, make sure all specified image attributes are correct
+        /// and correspond to unmanaged memory buffer. If some attributes are specified incorrectly,
+        /// this may lead to exceptions working with the unmanaged memory.</note></para></remarks>
+        /// 
         public UnmanagedImage( IntPtr imageData, int width, int height, int stride, PixelFormat pixelFormat )
         {
             this.imageData   = imageData;
@@ -85,6 +95,149 @@ namespace AForge.Imaging
             this.height      = height;
             this.stride      = stride;
             this.pixelFormat = pixelFormat;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnmanagedImage"/> class.
+        /// </summary>
+        /// 
+        /// <param name="bitmapData">Locked bitmap data.</param>
+        /// 
+        public UnmanagedImage( BitmapData bitmapData )
+        {
+            this.imageData   = bitmapData.Scan0;
+            this.width       = bitmapData.Width;
+            this.height      = bitmapData.Height;
+            this.stride      = bitmapData.Stride;
+            this.pixelFormat = bitmapData.PixelFormat;
+        }
+
+        /// <summary>
+        /// Destroys the instance of the <see cref="UnmanagedImage"/> class.
+        /// </summary>
+        /// 
+        ~UnmanagedImage( )
+        {
+            Dispose( false );
+        }
+
+        /// <summary>
+        /// Dispose the object.
+        /// </summary>
+        /// 
+        /// <remarks><para>Frees unmanaged resources used by the object. The object becomes unusable
+        /// after that.</para>
+        /// 
+        /// <par><note>The method needs to be called only in the case if unmanaged image was allocated
+        /// using <see cref="Create"/> method. In the case if the class instance was created using constructor,
+        /// this method does not free unmanaged memory.</note></par>
+        /// </remarks>
+        /// 
+        public void Dispose( )
+        {
+            Dispose( true );
+            // remove me from the Finalization queue 
+            GC.SuppressFinalize( this );
+        }
+
+        /// <summary>
+        /// Dispose the object.
+        /// </summary>
+        /// 
+        /// <param name="disposing">Indicates if disposing was initiated manually.</param>
+        /// 
+        protected virtual void Dispose( bool disposing )
+        {
+            if ( disposing )
+            {
+                // dispose managed resources
+            }
+            // free image memory if the image was allocated using this class
+            if ( ( mustBeDisposed ) && ( imageData != IntPtr.Zero ) )
+            {
+                System.Runtime.InteropServices.Marshal.FreeHGlobal( imageData );
+                imageData = IntPtr.Zero;
+            }
+        }
+
+        /// <summary>
+        /// Allocate new image in unmanaged memory.
+        /// </summary>
+        /// 
+        /// <param name="width">Image width.</param>
+        /// <param name="height">Image height.</param>
+        /// <param name="pixelFormat">Image pixel format.</param>
+        /// 
+        /// <returns>Return image allocated in unmanaged memory.</returns>
+        /// 
+        /// <remarks><para>Allocate new image with specified attributes in unmanaged memory.</para>
+        /// 
+        /// <para><note>The method support only
+        /// <see cref="System.Drawing.Imaging.PixelFormat">Format8bppIndexed</see>,
+        /// <see cref="System.Drawing.Imaging.PixelFormat">Format16bppGrayScale</see>,
+        /// <see cref="System.Drawing.Imaging.PixelFormat">Format24bppRgb</see>,
+        /// <see cref="System.Drawing.Imaging.PixelFormat">Format32bppArgb</see>,
+        /// <see cref="System.Drawing.Imaging.PixelFormat">Format48bppRgb</see> and
+        /// <see cref="System.Drawing.Imaging.PixelFormat">Format64bppArgb</see> pixel formats.
+        /// In the case if <see cref="System.Drawing.Imaging.PixelFormat">Format8bppIndexed</see>
+        /// format is specified, pallete is not not created for the image (supposed that it is
+        /// 8 bpp grayscale image).
+        /// </note></para>
+        /// </remarks>
+        /// 
+        /// <exception cref="ArgumentException">Unsupported pixel format was specified.</exception>
+        /// <exception cref="ArgumentException">Invalid image size was specified.</exception>
+        /// 
+        public static UnmanagedImage Create( int width, int height, PixelFormat pixelFormat )
+        {
+            int bytesPerPixel = 0 ;
+
+            // calculate bytes per pixel
+            switch ( pixelFormat )
+            {
+                case PixelFormat.Format8bppIndexed:
+                    bytesPerPixel = 1;
+                    break;
+                case PixelFormat.Format16bppGrayScale:
+                    bytesPerPixel = 2;
+                    break;
+                case PixelFormat.Format24bppRgb:
+                    bytesPerPixel = 3;
+                    break;
+                case PixelFormat.Format32bppArgb:
+                    bytesPerPixel = 4;
+                    break;
+                case PixelFormat.Format48bppRgb:
+                    bytesPerPixel = 6;
+                    break;
+                case PixelFormat.Format64bppArgb:
+                    bytesPerPixel = 8;
+                    break;
+                default:
+                    throw new ArgumentException( "Can not create image with specified pixel format." );
+            }
+
+            // check image size
+            if ( ( width <= 0 ) || ( height <= 0 ) )
+            {
+                throw new ArgumentException( "Invalid image size specified." );
+            }
+
+            // calculate stride
+            int stride = width * bytesPerPixel;
+
+            if ( stride % 4 != 0 )
+            {
+                stride += ( 4 - ( stride % 4 ) );
+            }
+
+            // allocate memory for the image
+            IntPtr imageData = System.Runtime.InteropServices.Marshal.AllocHGlobal( stride * height );
+
+            UnmanagedImage image = new UnmanagedImage( imageData, width, height, stride, pixelFormat );
+            image.mustBeDisposed = true;
+
+            return image;
         }
     }
 }
