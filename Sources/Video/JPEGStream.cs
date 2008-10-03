1a15f969dac1d9121c3fb850819e1f57a5b52870
andrew.kirillov <andrew.kirillov@ab5b2c17-5f1b-0410-93cd-b5c26ce635c7>
2008-10-03 12:42:29 +0000

    Minor update to documentation.
    git-svn-id: http://aforge.googlecode.com/svn/branches/2.0@729 ab5b2c17-5f1b-0410-93cd-b5c26ce635c7

--- Sources/Video/JPEGStream.cs
+++ Sources/Video/JPEGStream.cs
@@ -291,13 +291,15 @@ namespace AForge.Video
         /// object creates background thread and notifies about new frames with the
         /// help of <see cref="NewFrame"/> event.</remarks>
         /// 
+        /// <exception cref="ArgumentException">Video source is not specified.</exception>
+        /// 
         public void Start( )
 		{
 			if ( thread == null )
 			{
                 // check source
                 if ( ( source == null ) || ( source == string.Empty ) )
-                    throw new ArgumentException( "Video source is not specified" );
+                    throw new ArgumentException( "Video source is not specified." );
 
 				framesReceived = 0;
 				bytesReceived = 0;
