4b27b92f3b3e9e9dce85a2e579ab8dd7105a9080
admin@franknagl.de <admin@franknagl.de@ab5b2c17-5f1b-0410-93cd-b5c26ce635c7>
2008-11-13 19:06:31 +0000

    Wrong URL to server was corrected. Clean up the code. Commented the code. Tested the AutoUpdaterDemo with private server files.
    git-svn-id: http://aforge.googlecode.com/svn/branches/frank.nagl@783 ab5b2c17-5f1b-0410-93cd-b5c26ce635c7

--- Samples/AutoUpdaterDemo/MainForm.Designer.cs
+++ Samples/AutoUpdaterDemo/MainForm.Designer.cs
@@ -37,9 +37,10 @@
             // 
             // updateButton
             // 
-            this.updateButton.Location = new System.Drawing.Point(106, 106);
+            this.updateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateButton.Location = new System.Drawing.Point(106, 136);
             this.updateButton.Name = "updateButton";
-            this.updateButton.Size = new System.Drawing.Size(75, 23);
+            this.updateButton.Size = new System.Drawing.Size(105, 23);
             this.updateButton.TabIndex = 0;
             this.updateButton.Text = "Update";
             this.updateButton.UseVisualStyleBackColor = true;
@@ -48,7 +49,7 @@
             // 
             // checkUpdateButton
             // 
-            this.checkUpdateButton.Location = new System.Drawing.Point(106, 56);
+            this.checkUpdateButton.Location = new System.Drawing.Point(106, 66);
             this.checkUpdateButton.Name = "checkUpdateButton";
             this.checkUpdateButton.Size = new System.Drawing.Size(105, 23);
             this.checkUpdateButton.TabIndex = 1;
@@ -68,10 +69,11 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Lavender;
-            this.ClientSize = new System.Drawing.Size(292, 266);
+            this.ClientSize = new System.Drawing.Size(316, 266);
             this.Controls.Add(this.checkUpdateButton);
             this.Controls.Add(this.updateButton);
             this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AutoUpdaterDemo";
             this.ResumeLayout(false);
 
--- Samples/AutoUpdaterDemo/MainForm.cs
+++ Samples/AutoUpdaterDemo/MainForm.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using AForge.AutoUpdater;
+using System.IO;
 
 namespace AutoUpdaterDemo
 {
@@ -41,7 +42,19 @@ namespace AutoUpdaterDemo
         private void checkUpdateButton_Click(object sender, EventArgs e)
         {
             if (!updater.Usercheck())
-                MessageBox.Show("No update available.");
+            {
+                if (MessageBox.Show("No update available. " +
+                    "Should I delete the update (so you can update again)?",
+                    "No update available",
+                    MessageBoxButtons.OKCancel) == DialogResult.OK)
+                {
+                    WriteLine("version.txt", 1, "Version;1.0", true);
+                    WriteLine("version.txt", 2, "ReleaseDate;2008-11-13", true);
+                    File.Delete("UpdatedFile.txt");
+                    File.CreateText("IWillBeDeletedAfterUpdate.txt");
+                    MessageBox.Show("Update is retracted. You can update again.");
+                };
+            }
         }
 
         /// <summary>
@@ -54,6 +67,56 @@ namespace AutoUpdaterDemo
             if (MessageBox.Show("Do you want to update?", "Make update?",
                 MessageBoxButtons.OKCancel) == DialogResult.OK)
                 updater.MakeUpdate();
+        }
+
+        /// <summary>
+        /// Writes the passed text in the passed line of the file.
+        /// </summary>
+        /// <param name="filename">The filename.</param>
+        /// <param name="line">The line number.</param>
+        /// <param name="text">The text.</param>
+        /// <param name="replace">Replace text (t) or paste (f)</param>
+        private static void WriteLine(String filename, int line, string text, bool replace)
+        {
+            string content = "";
+            string[] delimiter = { "\r\n" };
+
+            if (File.Exists(filename))
+            {
+                StreamReader reader = new StreamReader(filename, System.Text.Encoding.Default);
+                content = reader.ReadToEnd();
+                reader.Close();
+            }
+
+            string[] cols = content.Split(delimiter, StringSplitOptions.None);
+
+            if (cols.Length >= line)
+            {
+                if (!replace)
+                    cols[line - 1] = text + "\r\n" + cols[line - 1];
+                else
+                    cols[line - 1] = text;
+
+                content = "";
+                for (int x = 0; x < cols.Length - 1; x++)
+                {
+                    content += cols[x] + "\r\n";
+                }
+                content += cols[cols.Length - 1];
+
+            }
+            else
+            {
+                for (int x = 0; x < line - cols.Length; x++)
+                    content += "\r\n";
+
+                content += text;
+            }
+
+
+            StreamWriter writer = new StreamWriter(filename);
+            writer.Write(content);
+            writer.Close();
         }
     }
 }
