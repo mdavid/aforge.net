82282f97f47be0f1a7652e24c998f55bfba7fc6c
nagl.frank <nagl.frank@ab5b2c17-5f1b-0410-93cd-b5c26ce635c7>
2008-11-13 14:10:02 +0000

    AutoUpdater added.
    git-svn-id: http://aforge.googlecode.com/svn/branches/frank.nagl@780 ab5b2c17-5f1b-0410-93cd-b5c26ce635c7

---
+++ Sources/AutoUpdater/Updater2/Updater2.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AForge.AutoUpdater
+{
+
+    /// <summary>
+    /// Finishs the update processes of the <code>Updater</code> class.
+    /// </summary>
+    public static class Updater2
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main(string []args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            new UpdateForm(args[0], args[1], args[2]);
+            Console.WriteLine("0:" + args[0]);
+            Console.WriteLine("1:" + args[1]);
+            Console.WriteLine("2:" + args[2]);
+        }
+
+        private class UpdateForm : Form
+        {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="UpdateForm"/> class.
+            /// </summary>
+            /// <param name="updateFile">The update file.</param>
+            /// <param name="programExe">The program exe.</param>
+            /// <param name="programFile">The program file.</param>
+            public UpdateForm(string updateFile, string programExe, string programFile)
+            {
+                ProgressBar bar = InitFormAndProgressBar();
+                System.Threading.Thread.Sleep(3000);
+
+                string updateDir = updateFile.Remove(updateFile.LastIndexOf('/') + 1);
+                StreamReader r = new StreamReader(updateFile);
+                List<string> copyFiles = new List<string>();
+                List<string> deleteFiles = new List<string>();
+
+                //Sets the new version id
+                string s = r.ReadLine();                
+                WriteLine(programFile, 1, s, true);
+
+                //Sets the new release date
+                s = r.ReadLine();
+                WriteLine(programFile, 2, s, true);
+
+                while ((s = r.ReadLine()) != null)
+                {
+                    if (s.StartsWith("Copy"))
+                        copyFiles.Add(s.Substring(s.IndexOf(';') + 1));
+                    if (s.StartsWith("Delete"))
+                        deleteFiles.Add(s.Substring(s.IndexOf(';') + 1));
+                }
+
+                r.Close();
+                bar.Value = copyFiles.Count;
+                bar.Maximum = 2 * copyFiles.Count;
+
+                //Deletes all deprecated files
+                for (int i = 0; i < deleteFiles.Count; i++)
+                {
+                    File.Delete(deleteFiles[i]);
+                }
+
+                //copy all new files into the Screenshotz dir
+                for (int i = 0; i < copyFiles.Count; i++)
+                {
+                    File.Copy(updateDir + copyFiles[i], copyFiles[i], true);
+                    bar.PerformStep();
+                }
+                copyFiles.Clear();
+                deleteFiles.Clear();
+                Directory.Delete(updateDir, true);
+                System.Windows.Forms.Application.OpenForms[0].Dispose();
+                //Close();//not necessary
+
+                //Restart updated program
+                if (MessageBox.Show("                     Update finished.",
+                                    "Update                                     ",
+                    MessageBoxButtons.OK) == DialogResult.OK)
+                {
+                    System.Diagnostics.ProcessStartInfo startInfo =
+                        new System.Diagnostics.ProcessStartInfo(programExe);
+                    System.Diagnostics.Process process =
+                        System.Diagnostics.Process.Start(startInfo);
+                }
+                Console.WriteLine("ende: starte " + programExe);
+                System.Threading.Thread.Sleep(5000);
+            }
+
+            private static ProgressBar InitFormAndProgressBar()
+            {
+                Form updateForm = new Form();
+                ProgressBar bar = new ProgressBar();
+                Label label = new Label();
+                updateForm.Controls.Add(bar);
+                updateForm.Controls.Add(label);
+
+                //init the form
+                updateForm.Text = "Updating...";
+                updateForm.Width = 300;
+                updateForm.Height = 200;
+                updateForm.BackColor = System.Drawing.Color.Lavender;
+                updateForm.StartPosition =
+                    System.Windows.Forms.FormStartPosition.CenterScreen;
+
+                //init the label
+                label.Text = "Updating...";
+                label.AutoSize = true;
+                label.Location = new System.Drawing.Point(100, 30);
+
+                //init the progressBar
+                bar.Location = new System.Drawing.Point(100, 70);
+                bar.Value = 5;//dummy value
+                bar.Maximum = 10;//dummy value
+                bar.Step = 1;
+
+                updateForm.Show();
+                updateForm.Refresh();
+                return bar;
+            }
+
+            /// <summary>
+            /// Schreibt den übergebenen Text in eine definierte Zeile.
+            /// </summary>
+            /// <param name="filename">Pfad zur Datei</param>
+            /// <param name="line">Zeilennummer</param>
+            /// <param name="lines">Text für die übergebene Zeile</param>
+            /// <param name="replace">Text in dieser Zeile überschreiben (t) oder einfügen (f)</param>
+            private static void WriteLine(String filename, int line, string lines, bool replace)
+            {
+                string content = "";
+                string[] delimiter = { "\r\n" };
+
+                if (File.Exists(filename))
+                {
+                    StreamReader reader = new StreamReader(filename, System.Text.Encoding.Default);
+                    content = reader.ReadToEnd();
+                    reader.Close();
+                }
+
+                string[] cols = content.Split(delimiter, StringSplitOptions.None);
+
+                if (cols.Length >= line)
+                {
+                    if (!replace)
+                        cols[line - 1] = lines + "\r\n" + cols[line - 1];
+                    else
+                        cols[line - 1] = lines;
+
+                    content = "";
+                    for (int x = 0; x < cols.Length - 1; x++)
+                    {
+                        content += cols[x] + "\r\n";
+                    }
+                    content += cols[cols.Length - 1];
+
+                }
+                else
+                {
+                    for (int x = 0; x < line - cols.Length; x++)
+                        content += "\r\n";
+
+                    content += lines;
+                }
+
+
+                StreamWriter writer = new StreamWriter(filename);
+                writer.Write(content);
+                writer.Close();
+            }
+        }
+    }
+}
