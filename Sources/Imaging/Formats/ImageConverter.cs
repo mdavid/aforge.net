d1b316839b1613f53f83f83b52f217e12eb5c2bf
admin@franknagl.de <admin@franknagl.de@ab5b2c17-5f1b-0410-93cd-b5c26ce635c7>
2008-12-13 21:26:00 +0000

    ImageConverter added for converting image files from IImageDecoder type. JPGCodec as IImageDecoder added.
    git-svn-id: http://aforge.googlecode.com/svn/branches/frank.nagl@823 ab5b2c17-5f1b-0410-93cd-b5c26ce635c7

---
+++ Sources/Imaging/Formats/ImageConverter.cs
@@ -0,0 +1,162 @@
+﻿// AForge Image Formats Library
+// AForge.NET framework
+//
+// Copyright © Frank Nagl, 2008
+// admin@franknagl.de
+// www.franknagl.de
+//
+namespace AForge.Imaging.Formats
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Drawing.Imaging;
+    using System.IO;
+    /// <summary>
+    /// Provides several static functions to convert one or multiple image file(s) in a 
+    /// special image format.
+    /// </summary>
+    /// <remarks>
+    /// <para>Supported formats:</para>
+    /// <list type="enumeration">BMP</list>
+    /// <list type="enumeration">EMF</list>
+    /// <list type="enumeration">GIF</list>
+    /// <list type="enumeration">JPG</list>
+    /// <list type="enumeration">PNG</list>
+    /// <list type="enumeration">TIFF</list>
+    /// <list type="enumeration">WMF</list>
+    /// </remarks>
+    public static class ImageConverter
+    {
+        /// <summary>
+        /// Converts to.
+        /// </summary>
+        /// <param name="newFormat">The new image format.</param>
+        /// <param name="imageFile">The image file to convert.</param>
+        /// <param name="overwrite">if set to <c>true</c> [overwrite].</param>
+        /// <returns>True if the converting process was successful, otherwise false.</returns>
+        public static bool ConvertTo(IImageDecoder newFormat, string imageFile, bool overwrite)
+        {
+            return Core(imageFile, imageFile, newFormat, overwrite, 0, 0, 100);
+        }
+
+        #region Private helper functions
+        /// <summary>
+        /// Calcs the length of the bigger side.
+        /// </summary>
+        /// <param name="imageFile">The image file.</param>
+        /// <param name="biggerSideLength">Length of the bigger side of the image.</param>
+        /// <param name="w">The width.</param>
+        /// <param name="h">The height.</param>
+        private static void CalcBiggerSideLength(string imageFile, 
+                                                 int biggerSideLength,
+                                                 out float w,
+                                                 out float h)
+        {
+            w = h = 0;
+            try
+            {
+                Bitmap pic = new Bitmap(imageFile);
+                w = (float)pic.Width;
+                h = (float)pic.Height;
+                float ratio = w / h;
+                if (w > h)
+                {
+                    w = biggerSideLength;
+                    h = w / ratio;
+                }
+                else
+                {
+                    h = biggerSideLength;
+                    w = h * ratio;
+                }
+                pic.Dispose();
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        /// <summary>
+        /// Transforms an image with the passed variables adn saves it.
+        /// </summary>
+        /// <param name="originalFile">The original file.</param>
+        /// <param name="newFile">The new file.</param>
+        /// <param name="newFormat">The new format.</param>
+        /// <param name="overwrite">If <c>true</c> the
+        /// original file will be overwritten or deleted.</param>
+        /// <param name="width">The width.</param>
+        /// <param name="height">The height.</param>
+        /// <param name="jpgQuality">The JPG quality.</param>
+        /// <returns></returns>
+        private static bool Core
+            (String originalFile, 
+             String newFile, 
+             IImageDecoder newFormat,
+             bool overwrite,
+             int width, 
+             int height, 
+             byte jpgQuality
+             )
+        {
+            Bitmap pic = ImageDecoder.DecodeFromFile(originalFile);
+            if (width == 0 /*|| height == 0*/)
+            {
+                width = pic.Width;
+                height = pic.Height;
+            }
+            float ratio = (float)width / (float)height;
+            float h = (float)pic.Height;
+            float w = (float)pic.Width;
+            float xStart = 0f;
+            float yStart = 0f;
+            float wCorrect = w;
+            float hCorrect = h;
+            bool result;
+
+            if (w / h > ratio)
+            {
+                wCorrect = h * ratio;
+                xStart = (w - wCorrect) / 2.0f;
+            }
+            else if (w / h <= ratio)
+            {
+                hCorrect = w / ratio;
+                yStart = (h - hCorrect) / 2.0f;
+            }
+
+            RectangleF rec = new RectangleF(xStart, yStart, wCorrect, hCorrect);
+            Bitmap croppedPic = pic.Clone(rec, pic.PixelFormat);
+            pic.Dispose();
+            croppedPic = new Bitmap(croppedPic, width, height);
+            
+            MemoryStream ms = new MemoryStream();
+            croppedPic.Save(ms, ImageFormat.Bmp);
+            croppedPic.Dispose();
+            //MemoryReducer.ReduceMemoryUsage();
+
+            newFormat.Open(ms);
+            
+            //Create destination directory, if it doesn't exist already
+            int index = newFile.LastIndexOf('\\');
+            if (index != -1 && !Directory.Exists(newFile.Substring(0, index)))
+                Directory.CreateDirectory(newFile.Substring(0, index));
+
+            //Check, if same destination file name, but no overwriting allowed
+            if (newFile == originalFile.ToLower() && !overwrite)
+                newFile = newFile.Insert(newFile.LastIndexOf('.'), "_1");
+
+            result = newFormat.Save(null, ref newFile);
+
+            //Check, if different destination file names,
+            //..but overwriting needed --> then delete original file
+            if (newFile != originalFile.ToLower() && overwrite)
+                File.Delete(originalFile);
+
+            newFormat.Close(); 
+                                  
+            return result;
+        }
+        #endregion Private helper functions
+    }
+}
---
+++ Sources/Imaging/Formats/JPGCodec.cs
@@ -0,0 +1,253 @@
+﻿// AForge Image Formats Library
+// AForge.NET framework
+//
+// Copyright © Frank Nagl, 2008
+// admin@franknagl.de
+// www.franknagl.de
+//
+namespace AForge.Imaging.Formats
+{
+    using System;
+    using System.IO;
+    using System.Drawing;
+    using System.Drawing.Imaging;
+    using System.ComponentModel;
+
+    /// <summary>
+    /// Information about JPG image's frame.
+    /// </summary>
+    public sealed class JPGImageInfo : ImageInfo
+    {
+        // JPG quality, has to be a value between 0-100
+        private byte quality;
+        /// <summary>
+        /// JPG quality value of the image.
+        /// </summary>
+        /// 
+        /// <remarks><para>Value has to be between 1 and 100.</para></remarks>
+        /// 
+        [Category("JPG Info")]
+        public byte Quality
+        {
+            get { return quality; }
+            set { quality = value; }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JPGImageInfo"/> class.
+        /// </summary>
+        /// 
+        public JPGImageInfo() { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JPGImageInfo"/> class.
+        /// </summary>
+        /// 
+        /// <param name="width">Image's width.</param>
+        /// <param name="height">Image's height.</param>
+        /// <param name="bitsPerPixel">Number of bits per image's pixel.</param>
+        /// <param name="frameIndex">Frame's index.</param>
+        /// <param name="totalFrames">Total frames in the image.</param>
+        /// 
+        public JPGImageInfo(int width, int height, int bitsPerPixel, int frameIndex, int totalFrames) :
+            base(width, height, bitsPerPixel, frameIndex, totalFrames) { }
+
+        /// <summary>
+        /// Creates a new object that is a copy of the current instance. 
+        /// </summary>
+        /// 
+        /// <returns>A new object that is a copy of this instance.</returns>
+        /// 
+        public override object Clone()
+        {
+            JPGImageInfo clone = new JPGImageInfo(width, height, bitsPerPixel, frameIndex, totalFrames);
+
+            clone.quality = quality;
+
+            return clone;
+        }
+    }
+
+    /// <summary>
+    /// JPG image format decoder.
+    /// </summary>
+    public class JPGCodec : IImageDecoder
+    {
+        // stream with JPG encoded data
+        private Stream stream = null;
+        // information about images retrieved from header
+        private JPGImageInfo imageInfo = null;
+        // stream position pointing to beginning of data - right after header
+        private long dataPosition = 0;
+        private byte quality;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JPGCodec"/> class.
+        /// </summary>
+        /// <param name="quality">The quality of the jpeg codec.</param>
+        public JPGCodec(byte quality)
+        {
+            this.quality = quality;
+        }
+
+        /// <summary>
+        /// Decode first frame of JPG image.
+        /// </summary>
+        /// 
+        /// <param name="stream">Source stream, which contains encoded image.</param>
+        /// 
+        /// <returns>Returns decoded image frame.</returns>
+        /// 
+        /// <exception cref="FormatException">Not a JPG image format.</exception>
+        /// <exception cref="NotSupportedException">Format of the JPG image is not supported.</exception>
+        /// <exception cref="ArgumentException">The stream contains invalid (broken) JPG image.</exception>
+        /// 
+        public Bitmap DecodeSingleFrame(Stream stream)
+        {
+            return new Bitmap(stream);
+        }
+
+        /// <summary>
+        /// Open specified stream.
+        /// </summary>
+        /// 
+        /// <param name="stream">Stream to open.</param>
+        /// 
+        /// <returns>Returns number of images found in the specified stream.</returns>
+        /// 
+        /// <exception cref="FormatException">Not a JPG image format.</exception>
+        /// <exception cref="NotSupportedException">Format of the JPG image is not supported.</exception>
+        /// <exception cref="ArgumentException">The stream contains invalid (broken) JPG image.</exception>
+        ///
+        public int Open(Stream stream)
+        {
+            // close previous decoding
+            Close();
+
+            this.imageInfo = ReadHeader(stream);
+            this.imageInfo.Quality = quality;
+            this.stream = stream;
+            this.dataPosition = stream.Seek(0, SeekOrigin.Current);
+
+            return imageInfo.TotalFrames;
+        }
+
+        /// <summary>
+        /// Decode specified frame.
+        /// </summary>
+        /// 
+        /// <param name="frameIndex">Image frame to decode.</param>
+        /// <param name="imageInfo">Receives information about decoded frame.</param>
+        /// 
+        /// <returns>Returns decoded frame.</returns>
+        /// 
+        /// <exception cref="NullReferenceException">No image stream was opened previously.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Stream does not contain frame with specified index.</exception>
+        /// <exception cref="ArgumentException">The stream contains invalid (broken) JPG image.</exception>
+        /// 
+        public Bitmap DecodeFrame(int frameIndex, out ImageInfo imageInfo)
+        {
+            // check requested frame index
+            if (frameIndex != 0)
+            {
+                throw new ArgumentOutOfRangeException("Currently opened stream does not contain frame with specified index.");
+            }
+
+            // seek to the required frame
+            stream.Seek(dataPosition, SeekOrigin.Begin);
+
+            //// read required frame
+            Bitmap image = new Bitmap(stream);
+
+            // provide also frame information
+            imageInfo = (JPGImageInfo)this.imageInfo.Clone();
+
+            return image;
+        }
+
+        /// <summary>
+        /// Saves an image as a jpeg image.
+        /// </summary>
+        /// <param name="bitmap">The original bitmap.</param>
+        /// <param name="path">Path to which the image would be saved.</param>
+        /// <returns>True, if the saving process was successful, otherwise false.</returns>
+        public bool Save(Bitmap bitmap, ref string path)
+        {
+            return Save(bitmap, ref path, "jpg");
+        }
+
+        /// <summary>
+        /// Saves an image as a jpeg image.
+        /// </summary>
+        /// <param name="bitmap">The original bitmap.</param>
+        /// <param name="path">Path to which the image would be saved.</param>
+        /// <param name="extension">The extension of the jpeg format.</param>
+        /// <returns>
+        /// True, if the saving process was successful, otherwise false.
+        /// </returns>
+        public bool Save(Bitmap bitmap, ref string path, string extension)
+        {
+            if (imageInfo.Quality < 0 || imageInfo.Quality > 100)
+                throw new ArgumentOutOfRangeException("quality must be between 0 and 100.");
+
+            path = path.Substring(0, path.LastIndexOf('.') + 1) + extension;
+            try
+            { SaveJpeg(path, Bitmap.FromStream(stream), imageInfo.Quality); }
+            catch (Exception)
+            { return false; }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Close decoding of previously opened stream.
+        /// </summary>
+        /// 
+        /// <remarks><para>The method does not close stream itself, but just closes
+        /// decoding cleaning all associated data with it.</para></remarks>
+        /// 
+        public void Close()
+        {
+            stream = null;
+            imageInfo = null;
+        }
+
+        // Read and process JPG header. After the header is read stream pointer will
+        // point to data.
+        private JPGImageInfo ReadHeader(Stream stream)
+        {
+            Bitmap image = new Bitmap(stream);
+
+            // prepare image information
+            JPGImageInfo imageInfo = new JPGImageInfo(image.Width, image.Height, 24, 0, 1);
+            image.Dispose();
+
+            return imageInfo;
+        }
+
+        /// <summary>
+        /// Saves an image as a jpeg image, with the passed quality.
+        /// </summary>
+        /// <param name="path">Path to which the image would be saved.</param>
+        /// <param name="img">The image</param>
+        /// <param name="quality">An integer from 0 to 100, with 100 being the
+        /// highest quality</param>
+        private static void SaveJpeg(string path, Image img, int quality)
+        {
+            if (quality < 0 || quality > 100)
+                throw new ArgumentOutOfRangeException("quality must be between 0 and 100.");
+
+
+            // Encoder parameter for image quality
+            EncoderParameter qualityParam =
+                new EncoderParameter(Encoder.Quality, quality);
+            // Jpeg image codec
+            ImageCodecInfo jpegCodec = ImageCodecInfo.GetImageEncoders()[1];
+
+            EncoderParameters encoderParams = new EncoderParameters(1);
+            encoderParams.Param[0] = qualityParam;
+
+            img.Save(path, jpegCodec, encoderParams);
+        }
+    }
+}
