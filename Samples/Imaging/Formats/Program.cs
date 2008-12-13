e8baedfce8512488f1560bc6331f6397fe1aef9b
admin@franknagl.de <admin@franknagl.de@ab5b2c17-5f1b-0410-93cd-b5c26ce635c7>
2008-12-13 21:28:10 +0000

    Small console example application of the ImageConverter. Converts example image 'RCX-brick.png' into a jpeg with 100% quality.
    git-svn-id: http://aforge.googlecode.com/svn/branches/frank.nagl@824 ab5b2c17-5f1b-0410-93cd-b5c26ce635c7

---
+++ Samples/Imaging/Formats/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AForge_2._0_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AForge.Imaging.Formats.JPGCodec jpg =
+                new AForge.Imaging.Formats.JPGCodec(100);
+            AForge.Imaging.Formats.ImageConverter.ConvertTo(jpg, "RCX-brick.png", false);
+        }
+    }
+}
