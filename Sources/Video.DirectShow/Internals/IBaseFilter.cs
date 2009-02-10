47d4b86a31a00f8fac56f71fb6796ccba43fdaca
andrew.kirillov <andrew.kirillov@ab5b2c17-5f1b-0410-93cd-b5c26ce635c7>
2009-02-10 19:52:41 +0000

    Minor update to documentation.
    git-svn-id: http://aforge.googlecode.com/svn/branches/2.0@871 ab5b2c17-5f1b-0410-93cd-b5c26ce635c7

--- Sources/Video.DirectShow/Internals/IBaseFilter.cs
+++ Sources/Video.DirectShow/Internals/IBaseFilter.cs
@@ -115,7 +115,7 @@ namespace AForge.Video.DirectShow.Internals
         /// Retrieves the pin with the specified identifier.
         /// </summary>
         /// 
-        /// <param name="id">Pointer to a constant wide-character string that identifies the pinþ</param>
+        /// <param name="id">Pointer to a constant wide-character string that identifies the pin.</param>
         /// <param name="pin">Address of a variable that receives a pointer to the pin's IPin interface.</param>
         /// 
         /// <returns>Return's <b>HRESULT</b> error code.</returns>
