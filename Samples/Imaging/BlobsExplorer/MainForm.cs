0a80c6de813775d8c1823b8de30ac63e94c4024d
andrew.kirillov <andrew.kirillov@ab5b2c17-5f1b-0410-93cd-b5c26ce635c7>
2009-10-25 14:28:33 +0000

    Committing initial version of new sample application - Blobs Explorer. Allows to get information about blobs found by blob counting routine.
    git-svn-id: http://aforge.googlecode.com/svn/branches/2.1@1115 ab5b2c17-5f1b-0410-93cd-b5c26ce635c7

---
+++ Samples/Imaging/BlobsExplorer/MainForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using AForge.Imaging;
+
+namespace BlobsExplorer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm( )
+        {
+            InitializeComponent( );
+        }
+
+        // Exit from application
+        private void exitToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            this.Close( );
+        }
+
+        // Open file
+        private void openToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            if ( openFileDialog.ShowDialog( ) == DialogResult.OK )
+            {
+                try
+                {
+                    ProcessImage( (Bitmap) Bitmap.FromFile( openFileDialog.FileName ) );
+                }
+                catch
+                {
+                    MessageBox.Show( "Failed loading selected image file.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error );
+                }
+            }
+        }
+
+        // Process image
+        private void ProcessImage( Bitmap image )
+        {
+            int foundBlobsCount = blobsBrowser.SetImage( image );
+
+            blobsCountLabel.Text = string.Format( "Found blobs' count: {0}", foundBlobsCount );
+        }
+
+        // Blob was selected - display its information
+        private void blobsBrowser_BlobSelected( object sender, Blob blob )
+        {
+            propertyGrid.SelectedObject = blob;
+            propertyGrid.ExpandAllGridItems( );
+        }
+    }
+}
