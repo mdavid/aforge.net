34332dc0bbc431c2bc9b6382bb02876fb12c5795
andrew.kirillov <andrew.kirillov@ab5b2c17-5f1b-0410-93cd-b5c26ce635c7>
2010-03-14 16:40:00 +0000

    Adding some more unit test to recent features.
    git-svn-id: http://aforge.googlecode.com/svn/branches/2.2@1224 ab5b2c17-5f1b-0410-93cd-b5c26ce635c7

--- Unit Tests/AForge.Math.Tests/Geometry/LineStraighteningOptimizer.cs
+++ Unit Tests/AForge.Math.Tests/Geometry/LineStraighteningOptimizer.cs
@@ -4,7 +4,7 @@ using AForge;
 using AForge.Math.Geometry;
 using MbUnit.Framework;
 
-namespace AForge.Math.GeometryTests
+namespace AForge.Math.Geometry.Tests
 {
     [TestFixture]
     public class LineStraighteningOptimizer
@@ -15,37 +15,15 @@ namespace AForge.Math.GeometryTests
         [Row( new int[] { 0, 0, 10, 0, 10, 10 }, new int[] { 0, 0, 10, 0, 10, 10 } )]
         [Row( new int[] { 0, 0, 10, 0, 5, 1 }, new int[] { 0, 0, 10, 0, 5, 1 } )]
         [Row( new int[] { 0, 0, 10, 0, 10, 10, 5, 5 }, new int[] { 0, 0, 10, 0, 10, 10 } )]
-        [Row( new int[] { 0, 0, 10, 0, 10, 10, 6, 6 }, new int[] { 0, 0, 10, 0, 10, 10 } )]
-        [Row( new int[] { 0, 0, 10, 0, 10, 10, 4, 4 }, new int[] { 0, 0, 10, 0, 10, 10 } )]
-        [Row( new int[] { 5, 5, 0, 0, 5, 0, 10, 0, 10, 5, 10, 10 }, new int[] { 0, 0, 10, 0, 10, 10 } )]
-        [Row( new int[] { 6, 6, 0, 0, 6, 0, 10, 0, 10, 4, 10, 10 }, new int[] { 0, 0, 10, 0, 10, 10 } )]
+        [Row( new int[] { 0, 0, 10, 0, 10, 10, 4, 6 }, new int[] { 0, 0, 10, 0, 10, 10 } )]
+        [Row( new int[] { 0, 0, 10, 0, 10, 10, 6, 4 }, new int[] { 0, 0, 10, 0, 10, 10 } )]
+        [Row( new int[] { 0, 0, 10, 0, 10, 10, 7, 3 }, new int[] { 0, 0, 10, 0, 10, 10 } )]
+        [Row( new int[] { 0, 0, 10, 0, 10, 10, 8, 2 }, new int[] { 0, 0, 10, 0, 10, 10, 8, 2 } )]
+        [Row( new int[] { 4, 6, 0, 0, 5, 1, 10, 0, 10, 5, 10, 10 }, new int[] { 0, 0, 10, 0, 10, 10 } )]
+        [Row( new int[] { 6, 4, 0, 0, 6, -1, 10, 0, 9, 4, 10, 10 }, new int[] { 0, 0, 10, 0, 10, 10 } )]
         public void OptimizationTest( int[] coordinates, int[] expectedCoordinates )
         {
-            List<IntPoint> shape = new List<IntPoint>( );
-            List<IntPoint> expectedShape = new List<IntPoint>( );
-
-            // build a shape top optimize
-            for ( int i = 0, n = coordinates.Length / 2; i < n; i++ )
-            {
-                shape.Add( new IntPoint( coordinates[i * 2], coordinates[i * 2 + 1] ) );
-            }
-
-            // build a shape, which should be result of optimization
-            for ( int i = 0, n = expectedCoordinates.Length / 2; i < n; i++ )
-            {
-                expectedShape.Add( new IntPoint( expectedCoordinates[i * 2], expectedCoordinates[i * 2 + 1] ) );
-            }
-
-            List<IntPoint> optimizedShape = optimizer.OptimizeShape( shape );
-
-            // check number of points in result shape
-            Assert.AreEqual( expectedShape.Count, optimizedShape.Count );
-
-            // check that all points matches with expected
-            for ( int i = 0, n = optimizedShape.Count; i < n; i++ )
-            {
-                Assert.AreEqual( expectedShape[i], optimizedShape[i] );
-            }
+            ShapeOptimizerTestBase.TestOptimizer( coordinates, expectedCoordinates, optimizer );
         }
     }
 }
